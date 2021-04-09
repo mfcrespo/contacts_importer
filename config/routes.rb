30e6262de7cbb4bafe486f31aaaefb0aadfcef96
mfcrespo <mafe_crespo@hotmail.com>
2021-04-09 15:39:05 -0500

    Add contact controller and change in route to contact/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
   devise_for :users
   root "home#index"
-  get "users/index"
+  #get "users/index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
