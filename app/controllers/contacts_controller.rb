4c2e5cdf298be9bfd834b2ad05179778940552e1
mfcrespo <mafe_crespo@hotmail.com>
2021-04-10 01:36:29 -0500

    Add destroy method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,6 +3,17 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
   end
 
+  def destroy
+    @contact = Contact.find(params[:id])
+		if @contact.destroy
+		  flash[:success] = 'Object was successfully deleted.'
+		  redirect_to contacts_index_path
+		else
+		  flash[:error] = 'Something went wrong'
+		  redirect_to contacts_index_path
+		end
+  end
+
 
   def import
     Contact.import(params[:file], current_user)
