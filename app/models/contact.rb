76e6327cce239c6a173561772f85be298c532955
mfcrespo <mafe_crespo@hotmail.com>
2021-04-10 01:35:41 -0500

    Add name and birthday validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,17 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  before_save :sanitize_text
+  NAME_REGEX_VALID = /\A[a-zA-Z\s-]+\z/
+
+  validates_presence_of :name, :birthday, :phone, :address, :credit_card, :franchise, :email,
+                         message: "can't be blank"
+
+  validates :name, format: { with: NAME_REGEX_VALID, message: 'Name with special character arent allowed, only "-" is allowed' }
+  validate :valid_birthday
+
+  def valid_birthday
+    date = Date.iso8601(birthday)
+  end
 
   def self.import(file, user)
     CSV.foreach(file.path, headers: true) do |row|
@@ -14,5 +26,10 @@ class Contact < ApplicationRecord
                                     user_id: user.id)
       contact.update(contact_hash)
     end
-  end  
+  end
+
+  def sanitize_text
+    self.name = name.titleize
+    self.email = email.downcase
+  end
 end
